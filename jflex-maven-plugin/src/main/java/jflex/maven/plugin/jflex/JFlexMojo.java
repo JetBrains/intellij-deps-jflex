0ee5e7345d23c10b5c8464912f846b4bb77186a8
Codrin.Ogreanu <codrin.ogreanu@jetbrains.com>
2025-02-12 14:06:39 +0100

    Removed absolute skeleton path

--- jflex-maven-plugin/src/main/java/jflex/maven/plugin/jflex/JFlexMojo.java
+++ jflex-maven-plugin/src/main/java/jflex/maven/plugin/jflex/JFlexMojo.java
@@ -87,9 +87,7 @@ public class JFlexMojo extends AbstractMojo {
   private boolean dot;
 
   /** Use external skeleton file. */
-  @Parameter(
-      defaultValue =
-          "/home/Codrin.Ogreanu/IdeaProjects/jflex/jflex/src/main/jflex/kotlin_skeleton.nested")
+  @Parameter(defaultValue = "../../../jflex/src/main/jflex/kotlin_skeleton.nested")
   private File skeleton;
 
   /** Strict JLex compatibility. */
